9fffa60891c2c5f56890ac7e4ef502f318d301a7
Jeremias Herrera <jherrera.nqn@gmail.com>
2026-04-11 12:31:48 -0300

    catalogo de peliculas hecho

---
+++ tp1-react-peliculas/src/data/catalogo.js
@@ -0,0 +1,202 @@
+const catalogoBase = [
+  {
+    id: "1",
+    title: "Bohemian Rhapsody",
+    director: "Bryan Singer",
+    year: 2018,
+    genre: "Musical",
+    rating: 7.9,
+    type: "movie",
+    poster: "https://image.tmdb.org/t/p/w500/lHu1wtNaczFPGFDTrjCSzeLPTKN.jpg"
+  },
+  {
+    id: "2",
+    title: "Marty Supreme",
+    director: "Josh Safdie",
+    year: 2025,
+    genre: "Drama",
+    rating: 0.0, // Al ser un lanzamiento futuro o muy reciente, puedes manejar lógicas de "Sin calificar"
+    type: "movie",
+    poster: "https://image.tmdb.org/t/p/w500/3oXvN5k1l9xM2G0fXzZ2wL5hO1p.jpg" // Placeholder genérico o URL real si ya existe en TMDB
+  },
+  {
+    id: "3",
+    title: "Interstellar",
+    director: "Christopher Nolan",
+    year: 2014,
+    genre: "Ciencia Ficción",
+    rating: 8.7,
+    type: "movie",
+    poster: "https://image.tmdb.org/t/p/w500/gEU2QniE6E77NI6lCU6MxlNBvIx.jpg"
+  },
+  {
+    id: "4",
+    title: "Breaking Bad",
+    director: "Vince Gilligan",
+    year: 2008,
+    genre: "Drama",
+    rating: 9.5,
+    type: "serie",
+    poster: "https://image.tmdb.org/t/p/w500/ggFHVNu6YYI5L9pCfOacjizRGt.jpg"
+  },
+  {
+    id: "5",
+    title: "Parasite",
+    director: "Bong Joon Ho",
+    year: 2019,
+    genre: "Suspenso",
+    rating: 8.5,
+    type: "movie",
+    poster: "https://image.tmdb.org/t/p/w500/7IiTTgloJzvGI1TAYymCfbfl3vT.jpg"
+  },
+  {
+    id: "6",
+    title: "The Office",
+    director: "Greg Daniels",
+    year: 2005,
+    genre: "Comedia",
+    rating: 9.0,
+    type: "serie",
+    poster: "https://image.tmdb.org/t/p/w500/qWnJ1HNZ4y41OSm5ikwOWroOZgq.jpg"
+  },
+  {
+    id: "7",
+    title: "Mad Max: Fury Road",
+    director: "George Miller",
+    year: 2015,
+    genre: "Acción",
+    rating: 8.1,
+    type: "movie",
+    poster: "https://image.tmdb.org/t/p/w500/8tZYtuWezp8JbcsvHYO0O46tFbo.jpg"
+  },
+  {
+    id: "8",
+    title: "Dark",
+    director: "Baran bo Odar",
+    year: 2017,
+    genre: "Ciencia Ficción",
+    rating: 8.8,
+    type: "serie",
+    poster: "https://image.tmdb.org/t/p/w500/apbrbWs8M9lyOpJYU5WXrpFbk1Z.jpg"
+  },
+  {
+    id: "9",
+    title: "The Godfather",
+    director: "Francis Ford Coppola",
+    year: 1972,
+    genre: "Drama",
+    rating: 9.2,
+    type: "movie",
+    poster: "https://image.tmdb.org/t/p/w500/3bhkrj58Vtu7enYsRolD1fZdja1.jpg"
+  },
+  {
+    id: "10",
+    title: "Stranger Things",
+    director: "The Duffer Brothers",
+    year: 2016,
+    genre: "Ciencia Ficción",
+    rating: 8.7,
+    type: "serie",
+    poster: "https://image.tmdb.org/t/p/w500/x2LSRK2Cm7MZhjluni1msVJ3wDF.jpg"
+  },
+  {
+    id: "11",
+    title: "Superbad",
+    director: "Greg Mottola",
+    year: 2007,
+    genre: "Comedia",
+    rating: 7.6,
+    type: "movie",
+    poster: "https://image.tmdb.org/t/p/w500/ek8e8txUyUwd2HNXvnZ28q4BHEw.jpg"
+  },
+  {
+    id: "12",
+    title: "Severance",
+    director: "Dan Erickson",
+    year: 2022,
+    genre: "Suspenso",
+    rating: 8.7,
+    type: "serie",
+    poster: "https://image.tmdb.org/t/p/w500/kUuL0i2zPEn6x0pCrr1tXv0bLIn.jpg"
+  },
+  {
+    id: "13",
+    title: "The Matrix",
+    director: "Lana Wachowski, Lilly Wachowski",
+    year: 1999,
+    genre: "Ciencia Ficción",
+    rating: 8.7,
+    type: "movie",
+    poster: "https://image.tmdb.org/t/p/w500/f89U3ADr1oiB1s9GkdPOEpXUk5H.jpg"
+  },
+  {
+    id: "14",
+    title: "Succession",
+    director: "Jesse Armstrong",
+    year: 2018,
+    genre: "Drama",
+    rating: 8.9,
+    type: "serie",
+    poster: "https://image.tmdb.org/t/p/w500/7ywAABq242vT71f7y3A5P8Lq5y1.jpg"
+  },
+  {
+    id: "15",
+    title: "Avengers: Infinity War",
+    director: "Anthony Russo, Joe Russo",
+    year: 2018,
+    genre: "Acción",
+    rating: 8.4,
+    type: "movie",
+    poster: "https://image.tmdb.org/t/p/w500/7WsyChQLEftFiDOVTGkv3hFpyyt.jpg"
+  },
+  {
+    id: "16",
+    title: "The Sopranos",
+    director: "David Chase",
+    year: 1999,
+    genre: "Drama",
+    rating: 9.2,
+    type: "serie",
+    poster: "https://image.tmdb.org/t/p/w500/6vMawTQxr1z980wXJd4yq0xX2N3.jpg"
+  },
+  {
+    id: "17",
+    title: "Whiplash",
+    director: "Damien Chazelle",
+    year: 2014,
+    genre: "Drama",
+    rating: 8.5,
+    type: "movie",
+    poster: "https://image.tmdb.org/t/p/w500/7fn624j5lj3xTme2SgiLCeuedmO.jpg"
+  },
+  {
+    id: "18",
+    title: "Pulp Fiction",
+    director: "Quentin Tarantino",
+    year: 1994,
+    genre: "Suspenso",
+    rating: 8.9,
+    type: "movie",
+    poster: "https://image.tmdb.org/t/p/w500/d5iIlFn5s0ImszYzBPb8SPFPzT.jpg"
+  },
+  {
+    id: "19",
+    title: "The Last of Us",
+    director: "Craig Mazin, Neil Druckmann",
+    year: 2023,
+    genre: "Acción",
+    rating: 8.8,
+    type: "serie",
+    poster: "https://image.tmdb.org/t/p/w500/uKvVjHNqB5pSQp0rsxS15k0IeX7.jpg"
+  },
+  {
+    id: "20",
+    title: "Everything Everywhere All at Once",
+    director: "Daniel Kwan, Daniel Scheinert",
+    year: 2022,
+    genre: "Ciencia Ficción",
+    rating: 7.8,
+    type: "movie",
+    poster: "https://image.tmdb.org/t/p/w500/w3LxiVYdWWRvEVdn5RYq6jIqkb1.jpg"
+  }
+];
