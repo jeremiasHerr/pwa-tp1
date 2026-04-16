ca7b9998674d192a7f3f94ae67d5c76977b1f269
jeremiasHerr <jherrera.nqn@gmail.com>
2026-04-16 17:09:26 -0300

    interfaz para las validaciones del formulario terminado

--- tp1-react-peliculas/components/FormInput/FormInput.jsx
+++ tp1-react-peliculas/components/FormInput/FormInput.jsx
@@ -1,18 +1,18 @@
 import styles from "./FormInput.module.css";
 
-export default function FormInput({label, value, tipo="text",esSelect=false, opciones=[], min, max, nombre}) {
+export default function FormInput({label, value, tipo="text",esSelect=false, opciones=[], min, max, nombre, error}) {
     return (
         <div className={styles.inputContainer}>
             <label>{label}</label>
             {esSelect ? (
-                <select name={nombre} defaultValue={value} className={styles.field}>
+                <select name={nombre} defaultValue={value} className={`${styles.field} ${error ? styles.input-error : ""}`}>
                     {opciones.map((opcion) => (
                         <option key={opcion} value={opcion}>{opcion}</option>
                     ))
                     }
                 </select>
                 ) :
-                <input name={nombre} min={min} max={max} className={styles.field} type={tipo} defaultValue={value}></input>
+                <input name={nombre} min={min} max={max} className={`${styles.field} ${error ? styles[`input-error`] : ""}`} type={tipo} defaultValue={value}></input>
             }
         </div>
     )
